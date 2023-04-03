ac40fe2cfe817791ccc6eed4c223adb471d77a43
Danthemes <danthemes@gmail.com>
2023-04-03 15:25:57 +0300

    update

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,20 +1,30 @@
 import React from "react";
 import { useForm } from "react-hook-form";
-import { toast } from "react-toastify";
-import { db } from "../firebase/firebase";
+import { ToastContainer, toast } from "react-toastify";
+import { db, storage } from "../firebase/firebase";
 import { useAuth } from "../hooks/auth";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { addDoc, collection } from "firebase/firestore";
 
 const newPostSchema = yup.object().shape({
-  photo: yup.mixed().test(
-    "fileSize",
-    "Only documents up to 2MB are permitted.",
-    (files) =>
-      !files || // Check if `files` is defined
-      files.length === 0 || // Check if `files` is not an empty list
-      Array.from(files).every((file) => file.size <= 2_000_000)
-  ),
+  photo: yup
+    .mixed()
+    .test("required", "Required", (file) => {
+      if (file.length > 0) {
+        return true;
+      }
+      return false;
+    })
+    .test(
+      "fileSize",
+      "Only documents up to 2MB are permitted.",
+      (files) =>
+        !files || // Check if `files` is defined
+        files.length === 0 || // Check if `files` is not an empty list
+        Array.from(files).every((file) => file.size <= 2_000_000)
+    ),
   description: yup.string().required("Required"),
 });
 
@@ -32,15 +42,26 @@ const NewPost = () => {
   const handleNewPost = async (data) => {
     const { photo, description } = data;
 
-    console.log(data);
     try {
-      await addDoc(db, "posts", {
-        uid: user.id,
-        photo,
+      // Upload photo to storage
+      const storageRef = ref(
+        storage,
+        "photos/" + `${photo[0].name}_${Date.now()}`
+      );
+      await uploadBytes(storageRef, photo);
+
+      // Get download URL for photo
+      const photoUrl = await getDownloadURL(storageRef);
+
+      // Add post to db
+      await addDoc(collection(db, "posts"), {
+        uid: user.id, // undefined here
+        photo: photoUrl,
         description,
       });
       toast.success("Post added");
     } catch (error) {
+      console.log(error.message);
       toast.error(error.message);
     }
   };
@@ -62,6 +83,8 @@ const NewPost = () => {
 
         <button type="submit">Add Post</button>
       </form>
+
+      <ToastContainer />
     </div>
   );
 };
