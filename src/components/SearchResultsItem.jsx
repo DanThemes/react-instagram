8dd2bf051b0ef97683752ef5f7e34f8aa34f537d
Danthemes <danthemes@gmail.com>
2023-04-12 10:48:04 +0300

    update

--- src/components/SearchResultsItem.jsx
+++ src/components/SearchResultsItem.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import Avatar from "./Avatar";
+import { Link } from "react-router-dom";
 
 const SearchResultsItem = ({ user }) => {
   return (
-    <div className="search-results-item">
-      <div className="search-results-item-avatar">
-        <Avatar uid={user.uid} size="small" showUsername={false} />
+    <Link to={`/profile/${user.username}`}>
+      <div className="search-results-item">
+        <div className="search-results-item-avatar">
+          <Avatar uid={user.uid} size="small" showUsername={false} />
+        </div>
+        <div className="search-results-item-username">{user.username}</div>
       </div>
-      <div className="search-results-item-username">{user.username}</div>
-    </div>
+    </Link>
   );
 };
 
